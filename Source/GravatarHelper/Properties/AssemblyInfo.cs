272f6cd4454ee4e270a1831794272055218e1906
Johannes Kommer <jkommer@users.noreply.github.com>
2012-07-19 23:49:34 +0100

    - Add shared VersionInfo used to share version number information between unit tests and main project.

--- Source/GravatarHelper/Properties/AssemblyInfo.cs
+++ Source/GravatarHelper/Properties/AssemblyInfo.cs
@@ -22,17 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a9529424-ae7c-4219-ae39-1cf257a86f74")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-// Please note: GravatarHelper uses Semantic Versioning. Please see http://semver.org for more details.
-[assembly: AssemblyVersion("3.1.0")]
-[assembly: AssemblyFileVersion("3.1.0")]
+// Version info is specified in VersionInfo.cs.
